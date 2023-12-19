a62819e7d49c4288bc9cbce81217f015d11e67d3
Eorys <xuan2692659908@foxmail.com>
2023-12-19 09:39:27 +0800

    update code

---
+++ one_question/12/19.cpp
@@ -0,0 +1,21 @@
+#include "../../head-file.h"
+
+/*
+problme:
+输入: mat = [[10,20,15],[21,30,14],[7,16,32]]
+输出: [1,1]
+解释: 30 和 32 都是峰值，所以[1,1]和[2,2]都是可接受的答案。
+url:
+ */
+
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>> &mat) {
+        
+    }
+};
+
+int main() {
+
+    return 0;
+}
