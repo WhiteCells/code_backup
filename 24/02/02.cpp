02436234517f815ef2e92a1882ac3c2f894190af
Eorys <xuan2692659908@foxmail.com>
2024-02-06 22:23:48 +0800

    update code

--- 24/02/02.cpp
+++ 24/02/02.cpp
@@ -1,52 +1,43 @@
 #include "../../head-file.h"
 
 /*
-problme: 
+problme:
 
-url: 
+url: https://leetcode.cn/problems/stone-game-vi/
  */
 
 class Solution {
 public:
-    vector<int> numsGame(vector<int> &nums) {
-        int n = nums.size();
-        vector<int> res(n);
-        priority_queue<int> lower;
-        priority_queue<int, vector<int>, greater<>> upper;
-        long long mod = 1e9 + 7;
-        long long lowerSum = 0, upperSum = 0;
-        for (int i = 0; i < n; i++) {
-            int x = nums[i] - i;
-            if (lower.empty() || lower.top() >= x) {
-                lowerSum += x;
-                lower.push(x);
-                if (lower.size() > upper.size() + 1) {
-                    upperSum += lower.top();
-                    upper.push(lower.top());
-                    lowerSum -= lower.top();
-                    lower.pop();
-                }
+    int stoneGameVI(vector<int> &aliceValues, vector<int> &bobValues) {
+        vector<tuple<int, int, int>> values;
+        for (int i = 0; i < aliceValues.size(); ++i) {
+            values.emplace_back(aliceValues[i] + bobValues[i],
+                                aliceValues[i], bobValues[i]);
+        }
+        sort(values.begin(), values.end(), [](tuple<int, int, int> &a, tuple<int, int, int> &b) {
+            return get<0>(a) > get<0>(b);
+             });
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < aliceValues.size(); ++i) {
+            if (i % 2) {
+                sum2 += get<2>(values[i]);
             } else {
-                upperSum += x;
-                upper.push(x);
-                if (lower.size() < upper.size()) {
-                    lowerSum += upper.top();
-                    lower.push(upper.top());
-                    upperSum -= upper.top();
-                    upper.pop();
-                }
-            }
-            if ((i + 1) % 2 == 0) {
-                res[i] = (upperSum - lowerSum) % mod;
-            } else {
-                res[i] = (upperSum - lowerSum + lower.top()) % mod;
+                sum1 += get<1>(values[i]);
             }
         }
-        return res;
+        cout << sum1 << " " << sum2 << endl;
+        if (sum1 > sum2) {
+            return 1;
+        } else if (sum1 < sum2) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 };
 
 int main() {
-    
+    vector<int> a {1, 3}, b {2, 1};
+    cout << Solution().stoneGameVI(a, b) << endl;
     return 0;
 }
